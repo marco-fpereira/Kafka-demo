2b8c4756b447c5f64008e1da9dcf31fc1f8e6b92
marco-fpereira <marcoantonio_fp@usp.br>
2023-02-07 00:33:32 -0300

    basic project implementation

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Kafka-demo
 Demonstration of a Kotlin project that exemplifies how to send and receive Kafka messages programmatically 
+
+# Commands 
+Run Zookeper
+	bin/windows/zookeeper-server-start.bat config/zookeeper.properties
+
+Run Kafka
+	bin/windows/kafka-server-start.bat config/server.properties
+
+List topics
+	bin/windows/kafka-topics.bat --list --bootstrap-server localhost:9092
+
+Create consumer 
+	bin/windows/kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic ECOMMERCE_NEW_ORDER --from-beginning
---
+++ src/main/kotlin/br/com/alura/NewOrderMain.kt
@@ -0,0 +1,30 @@
+package br.com.alura
+
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import java.util.*
+
+const val TOPIC = "ECOMMERCE_NEW_ORDER"
+
+fun main() {
+    val properties = Properties()
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+
+    val kafkaProducer = KafkaProducer<String, String>(properties)
+
+    val record = ProducerRecord(TOPIC, "123", "456789")
+    kafkaProducer.send(record) { data, ex ->
+        if(ex != null) {
+            ex.printStackTrace()
+            return@send
+        }
+        println("${data.topic()} : " +
+                "\npartition ${data.partition()} " +
+                "\noffset ${data.offset()} " +
+                "\ntimestamp ${data.timestamp()}")
+    }.get()
+}
